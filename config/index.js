5f57ce8859423982deb0be478e289b20466722a6
carl <105107903+CarlBaa@users.noreply.github.com>
2022-09-09 02:39:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,41 +31,41 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "gays0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "广东",
+      province: "四川",
       // 所在城市
-      city: "肇庆",
+      city: "凉山",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://neumorphism.io/#926363"
     }, 
     {
-      name: "老婆1", 
+      name: "木子", 
       id: "",
       useTemplateId: "",
       province: "",
       city: "",
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://neumorphism.io/#926363"
     }, 
     {
-      name: "老婆2",
+      name: "2",
       id: "",
       useTemplateId: "",
       province: "",
       city: "",
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://neumorphism.io/#926363"
     }, 
     {
-      name: "老婆3", 
+      name: "3", 
       id: "",
       useTemplateId: "",
       province: "",
       city: "",
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://neumorphism.io/#926363"
     }, 
   ],
 
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "四川",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "凉山",
 
   /** 重要节日相关 */
 
@@ -106,10 +106,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "生日", "name": "木子姐", "year": "1996", "date": "09-02"},
+    {"type": "节日", "name": "日", "year": "2020", "date": "09-03"},
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "节日", "name": "日", "year": "2021", "date": "09-01"},
   ],
 
   /**
@@ -130,8 +130,7 @@ export const config = {
     {"keyword": "love_day", date: "2015-05-01"},
     // 结婚纪念日
     {"keyword": "marry_day", date: "2020-01-04"},
-    // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
