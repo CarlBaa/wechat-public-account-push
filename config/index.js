774b18baff32463d47b2f50e1e4f259353634f88
carl <105107903+CarlBaa@users.noreply.github.com>
2022-09-09 04:04:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ export const config = {
       // 所在省份
       province: "四川",
       // 所在城市
-      city: "射洪",
+      city: "遂宁",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://neumorphism.io/#926363"
     }, 
