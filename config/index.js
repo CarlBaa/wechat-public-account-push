222ccd3ab11ead13c889f180715ac79b6af972a4
carl <105107903+CarlBaa@users.noreply.github.com>
2022-09-09 03:06:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,33 +1,7 @@
 export const config = {
-  /**
-   * 公众号配置
-   */
+  
 
-  // 公众号APP_ID 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "",
-
-  // 公众号APP_SECRET 
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "",
-
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "老婆0",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
-   */
+ 
   USERS: [
     {
       // 想要发送的人的名字
@@ -44,11 +18,11 @@ export const config = {
       openUrl: "https://neumorphism.io/#926363"
     }, 
     {
-      name: "木子", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
+      name: "A", 
+      id: "ox2Jg6YDb2JdKfA3EIw8Ugd3Tzvk",
+      useTemplateId: "Cl5wGZYfMpFEfi-foYYXiM4KM3IyV_Rp-XO_XNL8Dks",
+      province: "四川",
+      city: "凉山",
       openUrl: "https://neumorphism.io/#926363"
     }, 
     {
@@ -80,9 +54,9 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "A",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "ox2Jg6YDb2JdKfA3EIw8Ugd3Tzvk",
     }, 
   ],
     
