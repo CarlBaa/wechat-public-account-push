7ccdcdef0b0efcaaa50020ad0fd6ca2e60eb022a
carl <105107903+CarlBaa@users.noreply.github.com>
2022-09-09 08:32:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,26 @@ export const config = {
       province: "四川",
       city: "凉山",
       openUrl: "https://neumorphism.io/#926363",
-    },   
+    },  
+    
+    
+    {
+      name: "A", 
+      id: "ox2Jg6YDb2JdKfA3EIw8Ugd3Tzvk",
+      useTemplateId: "XUC1BP9he3zEuY5_KUqTtqm-uTgYn6qPyzwFo1zbE00",
+      province: "四川",
+      city: "凉山",
+      openUrl: "https://neumorphism.io/#926363"
+    }, 
+    
+     {
+      name: "dark ¥side", 
+      id: "ox2Jg6dHcLbZ9f3i2JGghq0f289c",
+      useTemplateId: "XUC1BP9he3zEuY5_KUqTtqm-uTgYn6qPyzwFo1zbE00",
+      province: "四川",
+      city: "内江",
+      openUrl: "https://neumorphism.io/#926363"
+    }, 
   ],
   
   /** 默认地域 */
